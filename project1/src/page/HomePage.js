d027fff3763aebe6675d6196f5b6be312ece9fea
Maryam Ghiasi <maryamghiasi2322@gmail.com>
2021-08-12 06:42:08 +0100

    builde home page

---
+++ project1/src/page/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Csvaval from "../component/Csvaval";
+
+class HomePage extends React.Component{
+
+    render(){
+        return(
+            <>
+           <Csvaval/>
+        </>
+        )
+    }
+};
+export default HomePage;
